8417dcf6c231c07ba389ffdc80d37b3ae90c0342
YYaaao <yaoyangl@buffalo.edu>
2020-03-12 20:39:48 -0400

    Layout for Login and Signup

---
+++ signup_front.php
@@ -0,0 +1,79 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>UB TRADE</title>
+<link rel="stylesheet" href="css/bootstrap.min.css"><!-- bootstrap-CSS -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" /><!-- style.css -->
+<link rel="stylesheet" href="css/font-awesome.min.css" /><!-- fontawesome-CSS -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- header -->
+	<header>
+		<div class="w3ls-header"><!--header-one-->
+			<div class="w3ls-header-right">
+				<ul>
+					<li class="dropdown head-dpdn">
+						<a href="signin_front.php" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i> Sign In</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="clearfix"> </div>
+		</div>
+		<div class="container">
+			<div class="agile-its-header">
+				<div class="logo">
+					<h1><a href="index.html"><span>UB</span>Social</a></h1>
+				</div>
+				<div class="agileits_search">
+				<a class="post-w3layouts-ad" href="index.html">Back to Homepage</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</header>
+	<!-- //header -->
+	<!-- sign in form -->
+  <section>
+   <div id="agileits-sign-in-page" class="sign-in-wrapper">
+     <div class="agileinfo_signin">
+     <h3>Sign Up</h3>
+       <form action="register.php" method="post" >
+         <input type="text" name="Name" placeholder="Username" required="">
+         <input type="email" name="Email" placeholder="Email Address" required="">
+         <input type="password" name="password" placeholder="Password" required="">
+         <input type="password" name="confirmPassword" placeholder="Confirm Password" required="">
+         <div class="signin-rit">
+           <!-- <span class="agree-checkbox">
+             <label class="checkbox"><input type="checkbox" name="checkbox">I agree to your <a class="w3layouts-t" href="terms.html" target="_blank">Terms of Use</a> and <a class="w3layouts-t" href="privacy.html" target="_blank">Privacy Policy</a></label>
+           </span> -->
+         </div>
+         <input type="submit" value="Sign Up">
+       </form>
+     </div>
+   </div>
+ </section>
+	<!-- //sign in form -->
+	<!--footer section start-->
+		<footer>
+			<div class="agileits-footer-bottom text-center">
+			<div class="container">
+				<div class="w3-footer-logo">
+					<h1><a href="index.html"><span>UB</span>Social</a></h1>
+				</div>
+				<div class="copyrights">
+					<p> Design by  <label> CSE_442_TEAM</label></p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		</footer>
+        <!--footer section end-->
+
+
+</html>
